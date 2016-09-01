71a67bbccd4c06e871435bf2ab168945f56edbcc
huzhongyin <767915479@qq.com>
2016-09-01 13:39:08 +0800

    第二次提交

---
+++ demo_gitTest/demo_gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo_gitTest
+//
+//  Created by  huzhongyin on 16/9/1.
+//  Copyright © 2016年 LanGe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ demo_gitTest/demo_gitTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  demo_gitTest
+//
+//  Created by  huzhongyin on 16/9/1.
+//  Copyright © 2016年 LanGe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title=@"第二个导航";
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
